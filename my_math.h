be71ca6e20e84556ea1c7507ad2fd205c10de499
Egor <weqqu2014@yandex.ru>
2020-10-19 06:14:28 -0700

    Some refactoring

--- my_math.h
+++ my_math.h
@@ -4,84 +4,91 @@
 #include <cmath>
 #include <vector>
 #include <cassert>
+#include <cstdint>
 #include <iostream>
 
 
-template<size_t dimension_columns, size_t dimension_rows, typename Type> class Matrix;
+template<int64_t Columns, int64_t Rows, typename T>
+class Matrix;
 
-template <size_t dimension, typename Type>
+template <int64_t Dimension, typename T>
 struct Vector
 {
 	Vector()
-	{ for (size_t i(dimension); i--; s_data[i] = Type()); }
-
-	Type &operator[](const size_t index)
 	{
-		assert(index < dimension);
-		return s_data[index];
+		for (int64_t i = 0; i < Dimension; i++)
+			s_data[i] = T();
 	}
 
-	const Type &operator[](const size_t index) const
+	T &operator[] (const int64_t i)
 	{
-		assert(index < dimension);
-		return s_data[index];
+		assert(i < Dimension);
+		return s_data[i];
+	}
+
+	const T &operator[] (const int64_t i) const
+	{
+		assert(i < Dimension);
+		return s_data[i];
 	}
 
 private:
-	Type s_data[dimension];
+	T s_data[Dimension];
 };
 
-template <typename Type>
-struct Vector<2, Type>
+template <typename T>
+struct Vector<2, T>
 {
 	Vector()
-	: s_x(Type()), s_y(Type())
-	{ }
+	: s_x(T()), s_y(T())
+	{}
 
-	Vector(Type x, Type y)
+	Vector(T x, T y)
 	: s_x(x), s_y(y)
-	{ }
+	{}
 
-	template <typename U> explicit Vector<2, Type> (const Vector<2, U> &vector);
+	template <typename U>
+	Vector<2, T> (const Vector<2, U> &vector);
 
-	Type &operator[](const size_t index)
+	T &operator[] (const int64_t i)
 	{
-		assert(index < 2);
-		return index <= 0 ? s_x : s_y;
+		assert(i < 2);
+		return i <= 0 ? s_x : s_y;
 	}
 
-	const Type &operator[](const size_t index) const
+	const T &operator[] (const int64_t i) const
 	{
-		assert(index < 2);
-		return index <= 0 ? s_x : s_y;
+		assert(i < 2);
+		return i <= 0 ? s_x : s_y;
 	}
 
-	Type s_x, s_y;
+	T s_x, s_y;
 };
 
-template <typename Type>
-struct Vector<3, Type>
+template <typename T>
+struct Vector<3, T>
 {
 	Vector()
-	: s_x(Type()), s_y(Type()), s_z(Type())
+	: s_x(T()), s_y(T()), s_z(T())
 	{ }
 
-	Vector(Type x, Type y, Type z)
+	Vector(T x, T y, T z)
 	: s_x(x), s_y(y), s_z(z)
 	{ }
 
-	template <typename U> explicit Vector<3, Type>(const Vector<3, U> &vector);
+	template <typename U>
+	Vector<3, T> (const Vector<3, U> &vector);
 
-	Type &operator[](const size_t index)
+	T &operator[] (const int64_t i)
 	{
-		assert(index < 3);
-		return index <= 0 ? s_x : (1 == index ? s_y : s_z);
+		assert(i < 3);
+		return i <= 0 ? s_x : (1 == i ? s_y : s_z);
 	}
 
-	const Type &operator[](const size_t index) const
+	const T &operator[](const int64_t i) const
 	{
-		assert(index < 3);
-		return index <= 0 ? s_x : (1 == index ? s_y : s_z);
+		assert(i < 3);
+		return i <= 0 ? s_x : (1 == i ? s_y : s_z);
 	}
 
 	float norm()
@@ -89,221 +96,280 @@ struct Vector<3, Type>
 		return std::sqrt(s_x * s_x + s_y * s_y + s_z * s_z);
 	}
 
-	Vector<3, Type> &normalize(Type length = 1)
+	Vector<3, T> &normalize(T length = 1)
 	{
 		length = length * (length / norm());
 		return length;
 	}
 
-	Type s_x, s_y, s_z;
+	T s_x, s_y, s_z;
 };
 
-template<size_t dimension, typename Type>
-Type operator*(const Vector<dimension, Type> &left_hand_side, const Vector<dimension, Type> &right_hand_side)
+template<int64_t Dimension, typename T>
+T operator* (const Vector<Dimension, T> &left_hand_side, const Vector<Dimension, T> &right_hand_side)
 {
-	Type result = Type();
-	for (size_t i(dimension); i--; result += left_hand_side[i] * right_hand_side[i]);
+	T result = T();
+
+	for (int64_t i = 0; i < Dimension; i++)
+		result += left_hand_side[i] * right_hand_side[i];
+
 	return result;
 }
 
-template<size_t dimension, typename Type>
-Vector<dimension, Type> operator+(Vector<dimension, Type> left_hand_side, const Vector<dimension, Type> &right_hand_side)
+template<int64_t Dimension, typename T>
+Vector<Dimension, T> operator+ (Vector<Dimension, T> left_hand_side, const Vector<Dimension, T> &right_hand_side)
 {
-	for (size_t i(dimension); i--; left_hand_side[i] += right_hand_side[i]);
+	for (uint64_t i = 0; i < Dimension; i++)
+		left_hand_side[i] += right_hand_side[i];
+
 	return left_hand_side;
 }
 
-template<size_t dimension, typename Type>
-Vector<dimension, Type> operator-(Vector<dimension, Type> left_hand_side, const Vector<dimension, Type> &right_hand_side)
+template<int64_t Dimension, typename T>
+Vector<Dimension, T> operator- (Vector<Dimension, T> left_hand_side, const Vector<Dimension, T> &right_hand_side)
 {
-	for (size_t i(dimension); i--; left_hand_side[i] -= right_hand_side[i]);
+	for (int64_t i = 0; i < Dimension; i++)
+		left_hand_side[i] -= right_hand_side[i];
+
 	return left_hand_side;
 }
 
-template<size_t Dimension, typename Type, typename U>
-Vector<Dimension, Type> operator*(Vector<Dimension,Type> left_hand_side, const U &right_hand_side)
+template<int64_t Dimension, typename T, typename U>
+Vector<Dimension, T> operator* (Vector<Dimension, T> left_hand_side, const U &right_hand_side)
 {
-	for (size_t i(Dimension); i--; left_hand_side[i] *= right_hand_side);
+	for (int64_t i = 0; i < Dimension; i++)
+		left_hand_side[i] *= right_hand_side;
+
 	return left_hand_side;
 }
 
-template<size_t Dimension, typename Type, typename U>
-Vector<Dimension, Type> operator/(Vector<Dimension, Type> left_hand_side, const U &right_hand_side)
+template<int64_t Dimension, typename T, typename U>
+Vector<Dimension, T> operator/ (Vector<Dimension, T> left_hand_side, const U &right_hand_side)
 {
-	for (size_t i(Dimension); i--; left_hand_side[i] /= right_hand_side);
+	for (int64_t i = 0; i < Dimension; i++)
+		left_hand_side[i] /= right_hand_side;
+
 	return left_hand_side;
 }
 
-template<size_t length, size_t dimension, typename Type>
-Vector<length, Type> embed(const Vector<dimension, Type> &vector, Type fill = 1)
+template<int64_t Length, int64_t Dimension, typename T>
+Vector<Length, T> embed(const Vector<Dimension, T> &vector, T fill = 1)
 {
-	Vector<length, Type> result;
-	for (size_t i(length); i--; result[i] = (i < dimension ? vector[i] : fill));
+	Vector<Length, T> result;
+
+	for (int64_t i = 0; i < Length; i++)
+		result[i] = (i < Dimension ? vector[i] : fill);
+
 	return result;
 }
 
-template<size_t length, size_t dimension, typename Type>
-Vector<length, Type> projection(const Vector<dimension, Type> &vector)
+template<int64_t Length, int64_t Dimension, typename T>
+Vector<Length, T> projection(const Vector<Dimension, T> &vector)
 {
-	Vector<length, Type> result;
-	for (size_t i(length); i--; result[i] = vector[i]);
+	Vector<Length, T> result;
+
+	for (int64_t i = 0; i < Length; i++)
+		result[i] = vector[i];
+
 	return result;
 }
 
-template <typename Type> Vector<3, Type> cross(Vector<3, Type> first_vector, Vector<3, Type> second_vector)
+template <typename T>
+Vector<3, T> cross(Vector<3, T> first_vector, Vector<3, T> second_vector)
 {
-	return Vector<3, Type>(first_vector.s_y * second_vector.s_z - first_vector.s_z * second_vector.s_y,
-						   first_vector.s_z * second_vector.s_x - first_vector.s_x * second_vector.s_z,
-						   first_vector.s_x * second_vector.s_y - first_vector.s_y * second_vector.s_x);
+	return Vector<3, T>(first_vector.s_y * second_vector.s_z - first_vector.s_z * second_vector.s_y,
+					    first_vector.s_z * second_vector.s_x - first_vector.s_x * second_vector.s_z,
+						first_vector.s_x * second_vector.s_y - first_vector.s_y * second_vector.s_x);
 }
 
-template <size_t dimension, typename Type>
-std::ostream &operator<<(std::ostream &output, Vector<dimension, Type> &vector)
+template <int64_t Dimension, typename T>
+std::ostream &operator<< (std::ostream &output, Vector<Dimension, T> &vector)
 {
-	for(unsigned int i(0); i < dimension; i++)
+	for(int64_t i = 0; i < Dimension; i++)
 		output << vector[i] << " " ;
+
 	return output ;
 }
 
-template<size_t dimension, typename Type>
+template<int64_t Dimension, typename T>
 struct dt
 {
-	static Type det(const Matrix<dimension, dimension, Type> &source)
+	static T det(const Matrix<Dimension, Dimension, T> &source)
 	{
-		Type result(0);
-		for (size_t i(dimension); i--; result += source[0][i] * source.cofactor(0, i));
+		T result = 0;
+
+		for (int64_t i = 0; i < Dimension; i ++)
+			result += source[0][i] * source.cofactor(0, i);
+
 		return result;
 	}
 };
 
-template<typename Type>
-struct dt<1, Type>
+template<typename T>
+struct dt<1, T>
 {
-	static Type det(const Matrix<1, 1, Type> &source)
-	{ return source[0][0]; }
+	static T det(const Matrix<1, 1, T> &source)
+	{
+		return source[0][0];
+	}
 };
 
-template<size_t dimension_rows, size_t dimension_columns, typename Type>
+template<int64_t Rows, int64_t Columns, typename T>
 class Matrix
 {
-	Vector<dimension_columns, Type> rows[dimension_rows];
-
 public:
 	Matrix() = default;
 
-	Vector<dimension_columns, Type> &operator[](const size_t index)
+	Vector<Columns, T> &operator[] (const int64_t i)
 	{
-		assert(index < dimension_rows);
-		return rows[index];
+		assert(i < Rows);
+		return rows[i];
 	}
 
-	const Vector<dimension_columns, Type> &operator[](const size_t index) const
+	const Vector<Columns, T> &operator[] (const int64_t i) const
 	{
-		assert(index < dimension_rows);
-		return rows[index];
+		assert(i < Rows);
+		return rows[i];
 	}
 
-	Vector<dimension_rows, Type> column(const size_t index) const
+	Vector<Rows, T> column(const int64_t j) const
 	{
-		assert(index < dimension_columns);
-		Vector<dimension_rows, Type> result;
-		for (size_t i(dimension_rows); i--; result[i] = rows[i][index]);
+		assert(j < Columns);
+		Vector<Rows, T> result;
+
+		for (int64_t i = 0; i < Rows; i++)
+			result[i] = rows[i][j];
+
 		return result;
 	}
 
-	void set_column(size_t j, Vector < dimension_rows, Type> vector)
+	void set_column(int64_t j, Vector < Rows, T> vector)
 	{
-		assert(j < dimension_columns);
-		for (size_t i(dimension_rows); i--; rows[i][j] = vector[i]);
+		assert(j < Columns);
+
+		for (int64_t i = 0; i < Rows; i++)
+			rows[i][j] = vector[i];
 	}
 
-	static Matrix<dimension_rows, dimension_columns, Type> identity()
+	static Matrix<Rows, Columns, T>
+	identity()
 	{
-		Matrix<dimension_rows, dimension_columns, Type> result;
-		for (size_t i(dimension_rows); i--; )
-			for (size_t j(dimension_columns); j--; result[i][j] = (i == j));
+		Matrix<Rows, Columns, T> result;
+
+		for (int64_t i = 0; i < Rows; i++)
+			for (int64_t j = 0; j < Columns; j++)
+				result[i][j] = (i == j);
+
 		return result;
 	}
 
-	Type det() const
+	T det() const
 	{
-		return dt<dimension_columns, Type>::det(*this);
+		return dt<Columns, T>::det(*this);
 	}
 
-	Matrix<dimension_rows - 1, dimension_columns - 1, Type> get_minor(size_t row, size_t column) const
+	Matrix<Rows - 1, Columns - 1, T>
+	get_minor(int64_t row, int64_t column) const
 	{
-		Matrix<dimension_rows - 1, dimension_columns - 1, Type> result;
-		for (size_t i(dimension_rows - 1); i--; )
-			for (size_t j(dimension_columns - 1); j--; result[i][j] = rows[i < row ? i : i + 1][j < column ? j : j + 1]);
+		Matrix<Rows - 1, Columns - 1, T> result;
+
+		for (int64_t i = 0; i < Rows - 1; i++)
+			for (int64_t j = 0; j < Columns - 1; j++)
+				result[i][j] = rows[i < row ? i : i + 1][j < column ? j : j + 1];
+
 		return result;
 	}
 
-	Type cofactor(size_t row, size_t column) const
+	T cofactor(int64_t row, int64_t column) const
 	{
 		return get_minor(row, column).det() * ((row + column) % 2 ? -1 : 1);
 	}
 
-	Matrix<dimension_rows, dimension_columns, Type> adjugate_matrix() const
+	Matrix<Rows, Columns, T>
+	adjugate_matrix() const
 	{
-		Matrix<dimension_rows ,dimension_columns, Type> result;
-		for (size_t i(dimension_rows); i--; )
-			for (size_t j(dimension_columns); j--; result[i][j] = cofactor(i, j));
+		Matrix<Rows ,Columns, T> result;
+
+		for (int64_t i = 0; i < Rows; i++)
+			for (int64_t j = 0; j < Columns; j++)
+				result[i][j] = cofactor(i, j);
+
 		return result;
 	}
 
-	Matrix<dimension_rows, dimension_columns, Type> invert_transpose()
+	Matrix<Rows, Columns, T>
+	invert_transpose()
 	{
-		Matrix<dimension_rows ,dimension_columns, Type> result = adjugate_matrix();
-		Type temp = result[0] * rows[0];
+		Matrix<Rows ,Columns, T> result = adjugate_matrix();
+		T temp = result[0] * rows[0];
 		return result / temp;
 	}
 
-	Matrix<dimension_columns, dimension_rows, Type> invert()
-	{ return invert_transpose().transpose(); }
-
-	Matrix<dimension_columns, dimension_rows, Type> transpose()
+	Matrix<Columns, Rows, T>
+	invert()
 	{
-		Matrix<dimension_columns, dimension_rows, Type> result;
-		for (size_t i(dimension_rows); i--; result[i] = column(i));
+		return invert_transpose().transpose();
+	}
+
+	Matrix<Columns, Rows, T>
+	transpose()
+	{
+		Matrix<Columns, Rows, T> result;
+
+		for (int64_t i = 0; i < Rows; i++)
+			result[i] = column(i);
+
 		return result;
 	}
+
+private:
+	Vector<Columns, T> rows[Rows];
 };
 
-template<size_t dimension_rows, size_t dimension_columns, typename Type>
-Vector<dimension_rows, Type> operator*(const Matrix<dimension_rows, dimension_columns, Type> &left_hand_side, const Vector<dimension_columns, Type> &right_hand_side)
+template<size_t Rows, size_t Columns, typename T>
+Vector<Rows, T> operator* (const Matrix<Rows, Columns, T> &left_hand_side, const Vector<Columns, T> &right_hand_side)
 {
-	Vector<dimension_rows, Type> result;
-	for (size_t i(dimension_rows); i--; result[i] = left_hand_side[i] * right_hand_side);
+	Vector<Rows, T> result;
+
+	for (int64_t i = 0; i < Rows; i++)
+		result[i] = left_hand_side[i] * right_hand_side;
+
 	return result;
 }
 
-template<size_t R1, size_t C1, size_t C2, typename Type>
-Matrix<R1, C2, Type> operator*(const Matrix<R1, C1, Type> &left_hand_side, const Matrix<C1, C2, Type> &right_hand_side)
+template<int64_t R1, int64_t C1, int64_t C2, typename T>
+Matrix<R1, C2, T> operator* (const Matrix<R1, C1, T> &left_hand_side, const Matrix<C1, C2, T> &right_hand_side)
 {
-	Matrix<R1, C2, Type> result;
-	for (size_t i(R1); i--; )
-		for (size_t j(C2); j--; result[i][j] = left_hand_side[i] * right_hand_side.column(j));
+	Matrix<R1, C2, T> result;
+
+	for (int64_t i = 0; i < R1; i++)
+		for (int64_t j = 0; j < C2; j++)
+			result[i][j] = left_hand_side[i] * right_hand_side.column(j);
+
 	return result;
 }
 
-template<size_t dimension_rows, size_t dimension_columns, typename Type>
-Matrix<dimension_columns, dimension_rows, Type> operator/(Matrix<dimension_rows, dimension_columns, Type> left_hand_side, const Type &right_hand_side)
+template<int64_t Rows, int64_t Columns, typename T>
+Matrix<Columns, Rows, T> operator/ (Matrix<Rows, Columns, T> left_hand_side, const T &right_hand_side)
 {
-	for (size_t i(dimension_rows); i--; left_hand_side[i] = left_hand_side[i] / right_hand_side);
+	for (int64_t i = 0; i < Rows; i++)
+		left_hand_side[i] = left_hand_side[i] / right_hand_side;
+
 	return left_hand_side;
 }
 
-template<size_t dimension_rows, size_t dimension_columns, class Type>
-std::ostream &operator<<(std::ostream &output, Matrix<dimension_rows, dimension_columns, Type> &matrix)
+template<int64_t Rows, int64_t Columns, typename T>
+std::ostream &operator<< (std::ostream &output, Matrix<Rows, Columns, T> &matrix)
 {
-	for (size_t i(0); i < dimension_rows; i++)
+	for (int64_t i = 0; i < Rows; i++)
 		output << matrix[i] << std::endl;
+
 	return output;
 }
 
-using VectorTwoInt     = Vector<2, int>;
-using VectorTwoFloat   = Vector<2, float>;
-using VectorThreeInt   = Vector<3, int>;
-using VectorThreeFloat = Vector<3, float>;
-using VectorFourFloat  = Vector<4, float>;
+using VectorTwoInt     = Vector<2,    int>;
+using VectorThreeInt   = Vector<3,    int>;
+using VectorTwoFloat   = Vector<2,    float>;
+using VectorThreeFloat = Vector<3,    float>;
+using VectorFourFloat  = Vector<4,    float>;
 using MatrixFourByFour = Matrix<4, 4, float>;
